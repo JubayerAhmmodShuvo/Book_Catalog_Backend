3e3ee8ac0229c08547f33568a6aecd3fa9a897fb
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2023-09-27 15:48:46 +0600

    auth middleware modified

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,34 +1,48 @@
-import { NextFunction, Request, Response } from 'express';
-import httpStatus from 'http-status';
-import { Secret } from 'jsonwebtoken';
-import config from '../../config';
+/* eslint-disable no-unused-expressions */
+
+import { Request, Response, NextFunction } from 'express';
 import ApiError from '../../errors/ApiError';
-import { jwtHelpers } from '../../helpers/jwtHelpers';
+import httpStatus from 'http-status';
+import { jwtHelper } from '../../helpers/jwtHelpers';
+import config from '../../config';
+import { Secret } from 'jsonwebtoken';
+import { UserRole } from '@prisma/client';
 
-const auth =
-  (...requiredRoles: string[]) =>
-  async (req: Request, res: Response, next: NextFunction) => {
+// requestValidationHandler
+const authHandler =
+  (...selected_roles: string[]) =>
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-      //get authorization token
-      const token = req.headers.authorization;
+      //   check authorization
+      const token = req.headers?.authorization;
+
       if (!token) {
-        throw new ApiError(httpStatus.UNAUTHORIZED, 'You are not authorized');
+        throw new ApiError(httpStatus.UNAUTHORIZED, 'Unauthorized');
       }
-      // verify token
-      let verifiedUser = null;
 
-      verifiedUser = jwtHelpers.verifyToken(token, config.jwt.secret as Secret);
+      const decoded_user = jwtHelper.verify_token(
+        token,
+        config.jwt.access_token_secret as Secret
+      );
+      const { userId, email, role } = decoded_user;
+     // console.log(userId, email, role);
 
-      req.user = verifiedUser; // role  , userid
+      // set in req
+      req.logged_in_user = decoded_user;
 
-      // role diye guard korar jnno
-      if (requiredRoles.length && !requiredRoles.includes(verifiedUser.role)) {
-        throw new ApiError(httpStatus.FORBIDDEN, 'Forbidden');
+      //   check if the user is authenticated
+      if (!userId) {
+        throw new ApiError(httpStatus.UNAUTHORIZED, 'Unauthorized');
       }
+
+      //  check if the user has the required role
+      if (!selected_roles.includes(role)) {
+        throw new ApiError(httpStatus.FORBIDDEN, 'forbidden');
+      }
+
       next();
     } catch (error) {
       next(error);
     }
   };
-
-export default auth;
+export default authHandler;
