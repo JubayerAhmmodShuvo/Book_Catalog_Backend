bd5463d4a54f7c4b508ce8c57bd95eaaa13cbf7f
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2023-09-27 15:47:05 +0600

    routes folder added

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -1,13 +1,17 @@
 import express from 'express';
+import { AuthRoute } from '../app/modules/auth/auth.route';
+import { UserRoute } from '../app/modules/user/user.routes';
+import { ProfileRoute } from '../app/modules/profile/profile.routes';
 
 
 const router = express.Router();
 
-// const all_routes = [
-//   { path: '/auth', router: },
+const all_routes = [
+  { path: '/auth', router: AuthRoute },
+  { path: '/users', router: UserRoute },
+  { path: '/profile', router: ProfileRoute },
+];
 
-// ];
-
-// all_routes.map(item => router.use(item.path, item.router));
+all_routes.map(item => router.use(item.path, item.router));
 
 export default router;
